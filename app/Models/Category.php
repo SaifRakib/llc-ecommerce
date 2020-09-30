cb0defc8e0b47e8dd105cc27605735fc48f946cd
Rakib Hasan <saifrakib9@gmail.com>
2020-09-30 12:55:20 +0600

    Category model added

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,10 +1,21 @@
 <?php
 
-namespace App\\Models;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class \Category extends Model
+class Category extends Model
 {
-   
+   public $guarded = [];
+
+   public function parent_category():\Illuminate\Database\Eloquent\Model\Relation\BelongsTo
+  
+   {
+     return $this->belongsTo(__CLASS__);
+   }
+
+   public function child_category():\Illuminate\Database\Eloquent\Model\Relation\HasMany
+   {
+       return $this->hasMany(__CLASS__);
+   }
 }
